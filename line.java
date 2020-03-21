c68b06eff126b00751f19b68916437512cd94dd4
ihssan <ihsan_bashar@hotmail.com>
2020-03-21 20:09:41 +0200

    assignment

---
+++ line.java
@@ -0,0 +1,13 @@
+package programming;
+
+public class line
+{
+public static void main(String[] args) 
+{
+ int i;
+for(i=1;i<=8;i++)
+{
+System.out.print("*");
+}
+}
+}
---
+++ square.java
@@ -0,0 +1,18 @@
+package programming;
+
+public class square 
+{
+public static void main(String[] args) 
+{
+ int x;
+ int y;
+for(x=1; x<=5; x++)
+{
+for(y=1; y<=10; y++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+}
